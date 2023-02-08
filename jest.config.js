2830ef7ac0f2c7ced096dbf3716c1a7e029622e0
Eric McDaniel <mcdanieles@gmail.com>
2023-02-08 05:00:06 -0600

    Update to no ts-jest config syntax

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
   },
 };
